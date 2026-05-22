73b6e937d75aeee03d7414666e7c6998beb588cc
Suraj Chapagain <64243291+surajchapagain20@users.noreply.github.com>
2026-05-22 18:29:24 +0545

    Update password.php
    updated

--- password.php
+++ password.php
@@ -1,5 +1,5 @@
 <?php
 // Store the user credentials securely (for example, in a database or use encryption)
-define('USER_ID', 'surajchapagain2023@gmail.com');
-define('USER_PASSWORD', '#@Nepal@100#'); // Use a more secure password in production
+define('USER_ID', '<EMAIL ADDRESS>');
+define('USER_PASSWORD', '<PASSWORD>'); // Use a more secure password in production
 ?>
